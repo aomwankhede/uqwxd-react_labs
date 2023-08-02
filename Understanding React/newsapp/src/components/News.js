c7d176eb4ec552bc0d61c3b744a3e570c65ab808
aomwankhede <wankhede.aom@gmail.com>
2023-08-03 01:48:01 +0530

    Still Not Done

--- Understanding React/newsapp/src/components/News.js
+++ Understanding React/newsapp/src/components/News.js
@@ -17,12 +17,12 @@ export class News extends Component {
     pageSize: PropTypes.number,
     category: PropTypes.string,
   };
-  capitalize(word){
-    let newWord="";
-    newWord+=(word[0].toUpperCase());
-    let i=1;
-    for(i=1;i<word.length;i++){
-      newWord+=word[i];
+  capitalize(word) {
+    let newWord = "";
+    newWord += word[0].toUpperCase();
+    let i = 1;
+    for (i = 1; i < word.length; i++) {
+      newWord += word[i];
     }
     return newWord;
   }
@@ -34,7 +34,11 @@ export class News extends Component {
       page: 1,
       totalResults: 0,
     };
-    document.title=`WorldToday ${this.props.category=="general"?"":this.capitalize(this.props.category)}`
+    document.title = `WorldToday ${
+      this.props.category == "general"
+        ? ""
+        : this.capitalize(this.props.category)
+    }`;
   }
 
   async componentDidMount() {
@@ -74,57 +78,67 @@ export class News extends Component {
     });
     this.updateNews();
   };
-  fetchMoreData=async ()=>{
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b312a337adf142a2aa8ac9c1f422b392&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({loading:true});
-    let data = await fetch(url); //data is promise
-    let parsedData = await data.json();
-    this.setState({
-      articles: this.state.articles.concat(parsedData.articles),
-      loading: false,
-    });
-  }
+  fetchMoreData = async () => {
+    setTimeout(async () => {
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b312a337adf142a2aa8ac9c1f422b392&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+      this.setState({ loading: true });
+      let data = await fetch(url); //data is promise
+      let parsedData = await data.json();
+      this.setState({
+        articles: this.state.articles.concat(Array.from(parsedData.articles)),
+        loading: false,
+        page: this.state.page + 1,
+      });
+    }, 1000);
+  };
   render() {
     return (
       <div className="container my-3">
-        <h1 className="text-center">{`Top ${this.capitalize(this.props.category)} Headlines`}</h1>
+        <h1 className="text-center">{`Top ${this.capitalize(
+          this.props.category
+        )} Headlines`}</h1>
         {/* {this.state.loading ? <Spinner height="30px" width="30px" /> : ""} */}
         <InfiniteScroll
           dataLength={this.state.articles.length}
           next={this.fetchMoreData}
-          hasMore={Math.ceil(this.state.totalResults) / this.props.pageSize > this.state.page}
-          loader={this.state.loading ? <Spinner height="30px" width="30px" /> : ""}
+          hasMore={
+            Math.ceil(this.state.totalResults) / this.props.pageSize >=
+            this.state.page
+          }
+          loader={
+            !this.state.loading ? <Spinner height="30px" width="30px" /> : ""
+          }
         >
-        <div className="row">
-          {!this.state.loading &&
-            this.state.articles.map((element) => {
-              //Agr loading nahi hot aahe t dakhwa
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <NewsItem
-                    author={element.author}
-                    publishedAt={new Date(element.publishedAt).toGMTString()}
-                    title={
-                      element.title
-                        ? element.title.slice(0, 30)
-                        : "The title is not found,click on read more for further inspection"
-                    }
-                    description={
-                      element.description
-                        ? element.description.slice(0, 88)
-                        : "The description is not found,click on read more for further inspection"
-                    }
-                    imageUrl={
-                      !element.urlToImage
-                        ? "https://c.ndtvimg.com/2023-07/21g20hf8_-ajit-pawar-pti-650_650x400_05_July_23.jpg"
-                        : element.urlToImage
-                    }
-                    newsUrl={element.url}
-                  />
-                </div>
-              );
-            })}
-        </div>
+          <div className="row">
+            {!this.state.loading &&
+              this.state.articles.map((element) => {
+                //Agr loading nahi hot aahe t dakhwa
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <NewsItem
+                      author={element.author}
+                      publishedAt={new Date(element.publishedAt).toGMTString()}
+                      title={
+                        element.title
+                          ? element.title.slice(0, 30)
+                          : "The title is not found,click on read more for further inspection"
+                      }
+                      description={
+                        element.description
+                          ? element.description.slice(0, 88)
+                          : "The description is not found,click on read more for further inspection"
+                      }
+                      imageUrl={
+                        !element.urlToImage
+                          ? "https://c.ndtvimg.com/2023-07/21g20hf8_-ajit-pawar-pti-650_650x400_05_July_23.jpg"
+                          : element.urlToImage
+                      }
+                      newsUrl={element.url}
+                    />
+                  </div>
+                );
+              })}
+          </div>
         </InfiniteScroll>
         <div className="container d-flex justify-content-between">
           <button
